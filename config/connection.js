399c5b889ce91b65020e9b21a08b73598471426a
Martha Potts <marthajpotts7@gmail.com>
2021-07-03 04:00:34 -0400

    trying a different approach, deleted prior and added bare bones server and database, tested and functioning...at the moment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,14 @@
 const Sequelize = require('sequelize'); 
-require('dotenv').config(); 
+require('dotenv').config();
 
 let sequelize; 
+//for production/Heroku || local
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(proces.env.JAWSDB_URL); 
+    sequelize = new Sequelize(process.env.JAWSDB_URL); 
 } else {
     sequelize = new Sequelize(
         process.env.DB_NAME, 
-        process.env.DB_USER, 
+        process.env.DB_USER,
         process.env.DB_PASSWORD, 
         {
             host: 'localhost', 
@@ -19,9 +20,9 @@ if (process.env.JAWSDB_URL) {
 async function testConnection() {
     try {
         const auth = await sequelize.authenticate(); 
-        console.log('Connection to db established successfully'); 
+        console.log('Connection to database established successfully!'); 
     } catch (err) {
-        console.error('Unable to connect to db', err); 
+        console.error('Unable to connect to database', err); 
     }; 
 }
 testConnection(); 
---
+++ server.js
@@ -0,0 +1,29 @@
+const path = require('path'); 
+const express = require('express'); 
+const handlebars = require('express-handlebars'); 
+
+const sequelize = require('./config/connection'); 
+// const SequelizeStore = require('connect-session-sequelize')(session.Store); 
+
+const app = express(); 
+const PORT = process.env.PORT || 3001; 
+
+app.use(express.json()); 
+app.use(express.urlencoded( {extended: true} )); 
+
+app.use(express.static('public'));
+
+app.set('view engine', 'hbs'); 
+app.engine('hbs', handlebars({
+    extname: 'hbs', 
+    layoutsDir: __dirname + '/views/layouts', 
+    partialsDir: __dirname + '/views/partials', 
+})); 
+
+app.get('/', (req, res) => {
+    res.render('homepage'); 
+})
+
+sequelize.sync( {force: false} ).then( () => {
+    app.listen(PORT, () => console.log(`App listening on ${PORT}`)); 
+}); 
